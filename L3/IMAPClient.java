8b33ce79611e0e9f08117e586484e50ef3d7e04b
Alex Lindberg <alex.lindberg@nvrmind.se>
2023-01-02 14:48:57 +0100

    Refactoring

--- L3/IMAPClient.java
+++ L3/IMAPClient.java
@@ -1,30 +1,23 @@
 // package L3;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.security.KeyStore;
-import java.security.SecureRandom;
-import java.util.Arrays;
-import java.util.Enumeration;
 
 import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLSessionContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.SSLContext;
 
 public class IMAPClient implements AutoCloseable {
 
-    public static final String USERNAME = System.getenv("IMAP_USERNAME");
-    public static final String PASSWORD = System.getenv("IMAP_PASSWORD");
+    public static final String USERNAME = System.getenv("KTH_USERNAME");
+    public static final String PASSWORD = System.getenv("KTH_PASSWORD");
     public static final String HOST = "webmail.kth.se";
     public static final int PORT = 993;
     private int A_num;
@@ -34,28 +27,31 @@ public class IMAPClient implements AutoCloseable {
     private PrintWriter writer;
 
     public IMAPClient(SSLSocket socket) throws IOException {
-        try {
-            socket.setUseClientMode(true);
-            socket.startHandshake();
-        } catch (IOException e) {
-            System.out.println("Handshake error");
-            throw e;
-        }
-        System.out.println("Handshake Completed");
+        if (USERNAME == null)
+            throw new AssertionError("USERNAME not set as environment variable");
+        if (PASSWORD == null)
+            throw new AssertionError("PASSWORD not set as environment variable");
+    
         this.socket = socket;
+        this.socket.setUseClientMode(true);
+        this.socket.startHandshake();
         this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.writer = new PrintWriter(socket.getOutputStream());
         this.A_num = 0;
     }
 
-    public void send(String command) throws IOException, InterruptedException {
-        Thread.sleep(250);
+    public void exchange(String message) throws IOException, InterruptedException {
+        send(message);
+        Thread.sleep(200);
+        readAll();
+    }
+
+    public void send(String command) throws IOException {
         if (reader.ready())
             reader.readLine();
         String req = String.format("A%d %s\r", this.A_num, command);
-        if (command.contains("LOGIN")) { // dont print password
-            String[] t = Arrays.copyOf(req.split(" "), req.split(" ").length - 1);
-            System.out.println(String.join(" ", t) + " [myPassword]");
+        if (command.contains("LOGIN")) {
+            System.out.println(req.replace(PASSWORD, "[PASSWORD]"));
         } else
             System.out.println(req);
         writer.println(req);
@@ -74,57 +70,42 @@ public class IMAPClient implements AutoCloseable {
     }
 
     public void run() throws IOException, InterruptedException {
-        String[] commands = {
-                String.format("LOGIN %s %s", USERNAME, PASSWORD),
-                "EXAMINE INBOX",
-                "FETCH 1 BODY[TEXT]",
-                "LOGOUT"
-        };
         readAll();
-        for (String command : commands) {
-            send(command);
-            readAll();
-        }
+        exchange(String.format("LOGIN %s %s", USERNAME, PASSWORD));
+        exchange("EXAMINE INBOX");
+        exchange("FETCH 1 BODY[TEXT]");
+        exchange("LOGOUT");
     }
 
     @Override
     public void close() throws IOException {
-        this.reader.close();
-        this.writer.close();
-        this.socket.close();
+        if(this.reader != null) this.reader.close();
+        if(this.writer != null) this.writer.close();
+        if(this.socket != null) this.socket.close();
     }
 
-    private static SSLContext sslContext(String password) throws Exception {
-        KeyStore keystore = KeyStore.getInstance("PKCS12"); // rätt
+    private static SSLContext getSSLContext() throws Exception {
+        String password = System.getenv("KEYSTORE_PASSWORD");
+        if (password == null)
+            throw new AssertionError("KEYSTORE_PASSWORD not set as environment variable");
+        KeyStore keystore = KeyStore.getInstance("PKCS12");
         try (InputStream in = new FileInputStream("./alex.pfx")) {
             keystore.load(in, password.toCharArray());
         }
-
-        // System.out.println(java.util.Arrays.asList(keystore.));
         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm()); // fel
         keyManagerFactory.init(keystore, password.toCharArray());
-
         SSLContext sslContext = SSLContext.getInstance("SSL");
-        sslContext.init(
-                keyManagerFactory.getKeyManagers(),null, null);
+        sslContext.init(keyManagerFactory.getKeyManagers(),null, null);
 
         return sslContext;
     }
 
     public static void main(String[] args) throws Exception {
-        
-        SSLContext context = sslContext("password");
-        // String[] cipher = { "TLS_DHE_RSA_WITH_AES_128_CBC_SHA256" };
-        System.out.println(context.getProtocol());
+        SSLContext context = getSSLContext();
         SSLSocketFactory sf = (SSLSocketFactory) context.getSocketFactory();
         HttpsURLConnection.setDefaultSSLSocketFactory(sf);
-        String[] scs = sf.getSupportedCipherSuites();
-        for (int i = 0; i < scs.length; i++) {
-            System.out.println(scs[i]);
-        }
         
         try (SSLSocket sslSocket = (SSLSocket) sf.createSocket(HOST, PORT);) {
-            // sslSocket.setEnabledCipherSuites(scs);
             IMAPClient client = new IMAPClient(sslSocket);
             System.out.format("Connected with %s%n", sslSocket.getLocalAddress());
             client.run();
--- L3/SMTPClient.java
+++ L3/SMTPClient.java
@@ -1,16 +1,16 @@
+// package L3;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.KeyStore;
 import java.util.Arrays;
-import javax.net.ssl.HttpsURLConnection;
+
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
@@ -18,16 +18,11 @@ import javax.net.ssl.SSLSocketFactory;
 
 public class SMTPClient implements AutoCloseable {
 
-
-    public static final String USERNAME = System.getenv("SMTP_USERNAME");
-    public static final String PASSWORD = System.getenv("SMTP_PASSWORD");
     public static final String PLAIN = System.getenv("SMTP_PLAIN");
+    public static final String EMAIL = System.getenv("KTH_EMAIL");
     public static final String HOST = "smtp.kth.se";
     public static final int PORT = 587;
-    private static final String _OK = "250 ";
-    private static final String _READY = "220 ";
-    private static final String _AUTH = "235";
-    private static final String _END_DATA = "354 ";
+    private static final String[] stopCodes = { "220 ", "235 ", "250 ", "354 " };
 
     private Socket socket;
     private SSLSocket sslSocket;
@@ -35,19 +30,29 @@ public class SMTPClient implements AutoCloseable {
     private PrintWriter writer;
 
     public SMTPClient(Socket socket) throws IOException {
-        // socket.setUseClientMode(true);
+        if (PLAIN == null)
+            throw new AssertionError("PLAIN not set as environment variable");
+        if (EMAIL == null)
+            throw new AssertionError("EMAIL not set as environment variable");
         this.socket = socket;
-        // this.socket.setNeedClientAuth(false);
         this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.writer = new PrintWriter(socket.getOutputStream());
     }
 
-    public void send(String command) throws IOException, InterruptedException {
-        Thread.sleep(500);
-        if(reader.ready())
+    public void exchange(String message) throws IOException, InterruptedException {
+        send(message);
+        Thread.sleep(200);
+        readAll();
+    }
+
+    public void send(String message) throws IOException {
+        if (reader.ready())
             reader.readLine();
-        String req = String.format("%s\r", command);
-        System.out.println("C: " + req);
+        String req = String.format("%s\r", message);
+        if (message.contains("PLAIN")) { // obfuscate password
+            System.out.println(req.replace(PLAIN, "[B64_PLAIN]"));
+        } else
+            System.out.println("C: " + req);
         writer.println(req);
         writer.flush();
     }
@@ -56,7 +61,7 @@ public class SMTPClient implements AutoCloseable {
         String str;
         while ((str = reader.readLine()) != null) {
             System.out.println("S: " + str);
-            if (str.contains(_OK) || str.contains(_READY)|| str.contains(_AUTH) || str.contains(_END_DATA)) {
+            if (Arrays.stream(stopCodes).anyMatch(str::contains)) {
                 break;
             }
         }
@@ -64,77 +69,61 @@ public class SMTPClient implements AutoCloseable {
 
     public void run() throws Exception {
         Thread.sleep(250);
-        // readAll();
-        send(String.format("EHLO %s", HOST));
-        readAll();
-        
-        send("STARTTLS");
-        readAll();
-        SSLContext context = sslContext("password");
-        startTLS(socket, context);
-        
-        send(String.format("EHLO %s", HOST));
-        readAll();
-        send(String.format("AUTH PLAIN %s", PLAIN));
-        readAll();
-        
-        send("MAIL FROM:<alex5@kth.se>");
-        readAll();
-        send("RCPT TO:<alex.lindberg@nvrmind.se>");
-        readAll();
-        send("DATA");
-        readAll();
-        send("From: Alex\r\nTo: Me\r\nSubject: Smtp mail using sockets.\r\n\r\nThis is a message using smtp.\r\n.\r\n");
-        readAll();
-        send("QUIT");
-        readAll();
+        exchange(String.format("EHLO %s", HOST));
+        exchange("STARTTLS");
+
+        // Encryption step
+        SSLContext context = getSSLContext();
+        createSSLSocket(socket, context);
+
+        exchange(String.format("HELO %s", HOST));
+        exchange(String.format("AUTH PLAIN %s", PLAIN));
+        exchange(String.format("MAIL FROM:<%s>", EMAIL));
+        exchange("RCPT TO:<alex.lindberg@nvrmind.se>");
+        exchange("DATA");
+        exchange("From: Alex\r\n" +
+            "To: Me\r\n" +
+            "Subject: Smtp mail using sockets.\r\n\r\n" +
+            "This is a message using smtp.\r\n" +
+            ".\r\n");
+        exchange("QUIT");
     }
 
-    @Override
-    public void close() throws IOException {
-        this.reader.close();
-        this.writer.close();
-        this.socket.close();
-        this.sslSocket.close();
-    }
-
-    public void startTLS(Socket socket, SSLContext context) throws IOException, UnknownHostException, InterruptedException {
-        // String[] cipher = { "TLS_DHE_RSA_WITH_AES_128_CBC_SHA256" };
+    public void createSSLSocket(Socket socket, SSLContext context)
+            throws IOException, UnknownHostException, InterruptedException {
         SSLSocketFactory socketFactory = (SSLSocketFactory) context.getSocketFactory();
         SSLSocket SSLsocket = (SSLSocket) socketFactory.createSocket(socket, socket.getInetAddress().getHostAddress(),
-        socket.getPort(), true);
+                socket.getPort(), true);
         this.writer = new PrintWriter(SSLsocket.getOutputStream());
         this.reader = new BufferedReader(new InputStreamReader(SSLsocket.getInputStream()));
     }
 
-
-    private static SSLContext sslContext(String password) throws Exception {
-        KeyStore keystore = KeyStore.getInstance("PKCS12"); // rätt
+    private static SSLContext getSSLContext() throws Exception {
+        String password = System.getenv("KEYSTORE_PASSWORD");
+        if (password == null)
+            throw new AssertionError("KEYSTORE_PASSWORD not set as environment variable");
+        KeyStore keystore = KeyStore.getInstance("PKCS12");
         try (InputStream in = new FileInputStream("./alex.pfx")) {
             keystore.load(in, password.toCharArray());
         }
-
-        // System.out.println(java.util.Arrays.asList(keystore.));
-        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm()); // fel
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
         keyManagerFactory.init(keystore, password.toCharArray());
-
         SSLContext sslContext = SSLContext.getInstance("SSL");
-        sslContext.init(
-                keyManagerFactory.getKeyManagers(),null, null);
-
+        sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
         return sslContext;
     }
 
+    @Override
+    public void close() throws IOException {
+        if (this.reader != null) this.reader.close();
+        if (this.writer != null) this.writer.close();
+        if (this.socket != null) this.socket.close();
+        if (this.sslSocket != null) this.sslSocket.close();
+    }
 
     public static void main(String[] args) throws Exception {
-        // SSLContext context = sslContext("password");
-        // // String[] cipher = { "TLS_DHE_RSA_WITH_AES_128_CBC_SHA256" };
-        // System.out.println(context.getProtocol());
-        // SSLSocketFactory sf = (SSLSocketFactory) SSLSocketFactory.getDefault();
-        // HttpURLConnection.setDefaultSocketFactory(sf);
-        // try (SSLSocket sslSocket = (SSLSocket) sf.createSocket(HOST, PORT);
-        try(Socket socket = new Socket(HOST, PORT);
-            SMTPClient client = new SMTPClient(socket);) {
+        try (Socket socket = new Socket(HOST, PORT);
+                SMTPClient client = new SMTPClient(socket);) {
 
             System.out.format("Connected with %s%n", socket.getLocalAddress());
             client.run();
