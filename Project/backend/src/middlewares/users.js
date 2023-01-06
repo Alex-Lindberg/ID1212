637a18f204e99f4c5fa4e30ccde5e3f17d3c4a18
Alex Lindberg <alex.lindberg@nvrmind.se>
2023-01-06 01:54:10 +0100

    removed deprecated methods

--- Project/backend/src/middlewares/users.js
+++ Project/backend/src/middlewares/users.js
@@ -26,7 +26,7 @@ const getUserByEmail = async (req, res, next) => {
 const userExists = (boolean) => (req, res, next) => {
     if (!!res.locals.user !== boolean) {
         if (req.method === "POST" && req.url === '/api/users') {
-            return res.send(303, `User already exists`);
+            return res.status(303).send( `User already exists`);
         }
         return res.sendStatus(404);
     }
@@ -38,7 +38,7 @@ const getUser = async (req, res, next) => {
     try {
         const results = await query(`SELECT * FROM users WHERE id='${userId}'`);
         if (results.rows.length === 0)
-            return res.send(404, `No user found with id ${userId}`);
+            return res.status(404).send( `No user found with id ${userId}`);
         else {
             res.locals.user = results.rows[0];
             return next();
@@ -55,12 +55,10 @@ const createUser = async (req, res, next) => {
         const result = await query(
             `SELECT register_user('${username}', '${email}', '${password}')`
         );
-        console.log("result :>> ", JSON.stringify(result, null, 2));
         res.locals.user = result.rows[0];
         return next();
     } catch (error) {
-        console.log("error :>> ", error);
-        res.sendStatus(500);
+        return res.sendStatus(500);
     }
 };
 
