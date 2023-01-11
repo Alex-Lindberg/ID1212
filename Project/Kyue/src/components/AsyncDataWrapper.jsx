a0eda03e6f5196131661a21fca4ede0df632204a
Alex Lindberg <alex.lindberg@nvrmind.se>
2023-01-11 14:03:59 +0100

    Made array arguments possible

--- Project/Kyue/src/components/AsyncDataWrapper.jsx
+++ Project/Kyue/src/components/AsyncDataWrapper.jsx
@@ -1,11 +1,22 @@
+const checkData = (data) => {
+    if (typeof data === Array) {
+        return data.some((d) => !d);
+    }
+    return !data;
+};
+
+const checkError = (error) => {
+    if (typeof error === Array) return error.some((err) => err);
+    return error;
+};
+
 const AsyncDataWrapper = ({ data, error, children }) => {
-    if (!data) {
+    if (checkData(data)) {
         return <div className="async-loading">Loading...</div>;
     }
-    if (error) {
-        return <div className="async-error">Gick ej att hämta div</div>;
+    if (checkError(error)) {
+        return <div className="async-error">Something went wrong</div>;
     }
-    
     return children;
 };
 
